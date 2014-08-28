08ea8605c5af2c9ed50e255e389162a99b5486db
Geoff McLane <ubuntu@geffair.info>
2014-08-28 19:58:51 +0200

    move k nums test to its own module

---
+++ src/test-knums.hxx
@@ -0,0 +1,15 @@
+/*\
+ * test_knums.hxx
+ *
+ * Copyright (c) 2014 - Geoff R. McLane
+ * Licence: GNU GPL version 2
+ *
+\*/
+
+#ifndef _TEST_KNUMS_HXX_
+#define _TEST_KNUMS_HXX_
+
+extern void test_k_num();
+
+#endif // #ifndef _TEST_KNUMS_HXX_
+// eof - test_knums.hxx
--- src/test.cxx
+++ src/test.cxx
@@ -58,6 +58,7 @@
 #include "test-secs.hxx"
 #include "test-scanf.hxx"
 #include "test-open.hxx"
+#include "test-knums.hxx"
 
 extern void test_strtoimax();
 
@@ -378,59 +379,6 @@ void test_wait_for()
 }
 ////////////////////////////////////////////////////////////////////////////////////////////
 #endif // _MSC_VER
-
-TCHAR * get_k_num64( ULARGE_INTEGER uli )
-{
-   static TCHAR _s_kbuf64[264];
-   TCHAR * pb = _s_kbuf64;
-   double byts = ((double)uli.HighPart * 4294967296.0);
-   byts += uli.LowPart;
-   if( byts < 1024 ) {
-      sprintf(pb, "%f bytes", byts);
-   } else if( byts < 1024*1024 ) {
-      double ks = ((double)byts / 1024.0);
-      sprintf(pb, "%0.2f KB", ks);
-   } else if( byts < 1024*1024*1024 ) {
-      double ms = ((double)byts / (1024.0*1024.0));
-      sprintf(pb, "%0.2f MB", ms);
-   } else {
-      double gs = ((double)byts / (1024.0*1024.0*1024.0));
-      sprintf(pb, "%0.2f GB", gs);
-   }
-   return pb;
-}
-
-static char sbuff[1024];
-static char dbuff[1024];
-void test_k_num()
-{
-    char *cp;
-    long long tot = 2710732035;
-    unsigned long g_dir_count = 4568;
-    unsigned long g_file_clean = 75096;
-    ULARGE_INTEGER g_total64_size = { 0 };
-    g_total64_size.QuadPart = tot;
-    SPRTF("\n");
-    SPRTF("%s: test k numbers...\n", module);
-
-    sprintf(sbuff,"%llu", g_total64_size.QuadPart);
-    nice_num( dbuff, sbuff );
-    cp = get_k_num64( g_total64_size );
-    SPRTF( "Got %d dirs, %d files, total (approx) %s (%s bytes) ...\n",
-        g_dir_count,
-        g_file_clean, // (g_do_clean[0] ? g_file_clean : g_file_count),
-        cp,
-        dbuff );
-    g_total64_size.QuadPart = _UI64_MAX;
-    sprintf(sbuff,"%llu", g_total64_size.QuadPart);
-    nice_num( dbuff, sbuff );
-    cp = get_k_num64( g_total64_size );
-    SPRTF( "Max I64 total (approx) %s (%s bytes) ...\n",
-        cp,
-        dbuff );
-    SPRTF("%s: end test k numbers...\n", module);
-
-}
 
 #ifdef HAVE_SIMGEAR
 // we have simgear, so this can be compiled
