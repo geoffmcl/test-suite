6bbbb4f1c717095de8895b6d87179c65afbf282e
Geoff McLane <ubuntu@geffair.info>
2014-08-28 20:21:06 +0200

    beef up the rt (run test) batch file

--- build/rt.bat
+++ build/rt.bat
@@ -1,1 +1,19 @@
-release\test
+@setlocal
+@set TMPEXE=Release\test.exe
+@if NOT EXIST %TMPEXE% goto NOEXE
+@set TMPCMD=
+:RPT
+@if "%~1x" == "x" goto GOTCMD
+@set TMPCMD=%TMPCMD% %1
+@shift
+@goto RPT
+:GOTCMD
+
+%TMPEXE% %TMPCMD%
+
+@goto END
+
+:NOEXE
+@echo Can NOT find %TMPEXE%! Has it been built? *** FIX ME ***
+:END
+
