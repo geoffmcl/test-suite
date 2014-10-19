3384ec987d31b6d710bbebecbd9309ef54e24b6c
Geoff McLane <ubuntu@geffair.info>
2014-10-19 19:53:51 +0200

    add commands and 3rdpart PATH to batch

--- build/run-test.bat
+++ build/run-test.bat
@@ -3,10 +3,20 @@
 @if NOT EXIST %TMPEXE% goto NOEXE
 @set TMPPG=C:\Program Files (x86)\PostgreSQL\9.1\bin
 @if NOT EXIST "%TMPPG%" goto NOPG
+@set TMP3RD=C:\FG\17\3rdParty\bin
+@if NOT EXIST %TMP3RD%\nul goto NO3RD
 
-@set PATH=%TMPPG%;%PATH%
+@set TMPCMD=
+:RPT
+@if "%~1x" == "x" goto GOTCMD
+@set TMPCMD=%TMPCMD% %1
+@shift
+@goto RPT
+:GOTCMD
 
-%TMPEXE%
+@set PATH=%TMPPG%;%TMP3RD%;%PATH%
+
+%TMPEXE% %TMPCMD%
 
 @goto END
 
@@ -17,6 +27,10 @@
 
 :NOPG
 @echo Error: Can NOT locate %TMPPG%! *** FIX ME *** for your environment
+@goto END
+
+:NO3RD
+@echo Error: Can NOT locate %TMP3RD%! *** FIX ME *** for your environment
 @goto END
 
 :END
