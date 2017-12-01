c8d833d6adcfc12b5b55e764975217d18e6552c7
Geoff McLane <ubuntu@geoffair.info>
2017-12-01 20:33:58 +0100

    Make 'asprintf' implmentation optional

---
+++ test_conf.h.cmake
@@ -0,0 +1,13 @@
+/*
+   test_conf.h
+   Do **NOT** amend 'test_conf.h'!
+   Instead amend 'test_conf.h.cmake',
+   and run cmake configure again.
+ */
+#ifndef _TEST_CONF_H_
+#define _TEST_CONF_H_
+
+#cmakedefine HAVE_ASPRINTF 1
+
+#endif /* _TEST_CONF_H_ */
+/* eof test_conf.h.cmake -> test_conf.h */
